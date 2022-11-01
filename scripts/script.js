6f51eaf70f063712ef466d1b5e1ba20438f6b734
moensrc <rcmoens.rm@gmail.com>
2022-11-01 16:14:24 +0100

    Added some vars

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
-// JavaScript Document
-console.log("Howdy!");
+// Variables
+var leftButton = document.querySelector("header section button:first-of-type");
+var rightButton = document.querySelector("header section button:first-of-type");
+var year = document.querySelector("header section h2");
+var body = document.querySelector("body");
+
+
