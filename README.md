197c65dbf88c5c1c1dd024fb32ea2d99d9810780
moensrc <94112988+moensrc@users.noreply.github.com>
2022-11-02 19:43:39 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -36,7 +36,8 @@ Mijn startniveau is: zwart
   Aan deze technieken/punten wil ik werken:
   - Vormen en elementen maken in CSS
   - Interactie met JS
-  - Micro animaties
+  - Drie dimensionaliteit in CSS
+  - Micro animaties en micro interacties
  
 </details>
 
@@ -157,12 +158,46 @@ Mijn startniveau is: zwart
 ## Bronnenlijst
 
 <details open>
-<summary>continu bijhouden terwijl je werkt</summary>
+<summary>Bronnen</summary>
 
-Nb. Wees specifiek ('css-tricks' als bron is bijv. niet specifiek genoeg).
+<!-- Nb. Wees specifiek ('css-tricks' als bron is bijv. niet specifiek genoeg). -->
 
-1. bron 1
-2. bron 2
-3. ...
+1. Eigen border-radius creëren Generator (9Elements Github) https://9elements.github.io/fancy-border-radius/full-control.html#70.49.71.40-73.47.75.63-.
+
+2. Svg's in CSS (Tutorialspoint) https://www.tutorialspoint.com/How-to-draw-an-ellipse-in-HTML5-SVG
+
+3. Svg's in CSS (CSS-tricks) https://css-tricks.com/svg-properties-and-css/
+
+4. Ei vorm CSS (Lena Design) https://lenadesign.org/2021/03/15/css-egg-shape-and-css-easter-eggs/
+
+5. Ei vorm CSS (Codepen van Sanne) https://codepen.io/shooft/pen/OJEMxNV
+
+7. 3D Circular Carousel demo (3D transforms) https://3dtransforms.desandro.com/carousel
+
+8. Clip path animaties (CSS-tricks) https://css-tricks.com/animating-with-clip-path/
+
+9. Clippy voor custom clippath (Clippy | Bennett Feely) https://bennettfeely.com/clippy/
+
+10. Circular Carousel met jquery (Github repo) https://github.com/sgb-io/jquery.circular-carousel
+
+11. Transform values/properties (Mozilla) https://developer.mozilla.org/en-US/docs/Web/CSS/transform
+
+12. 3D Circular Carousel (Codepen) https://codepen.io/claudiulazar/pen/OdvVOo
+
+13. Grid guide (CSS-tricks) https://css-tricks.com/snippets/css/complete-guide-grid/
+
+14. RotateY uses (Mozilla) https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/rotateY
+
+15. Grid guide (Stackoverflow) https://stackoverflow.com/questions/18601932/css-3d-transform-perspective-grid
+
+16. CSS variabelen aanpassen met JS (CSS-tricks) https://css-tricks.com/updating-a-css-variable-with-javascript/
+
+17. Carousel (Codepen van Sanne) https://codepen.io/moensrc/pen/PoaNzeJ?editors=1100
+
+18.
+
+19.
+
+20.
 
 </details>
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,96 @@
 // Variables
 var leftButton = document.querySelector("header section button:first-of-type");
-var rightButton = document.querySelector("header section button:first-of-type");
-var year = document.querySelector("header section h2");
+var rightButton = document.querySelector("header section button:nth-of-type(2)");
+var yearText = document.querySelector("header section h2");
 var body = document.querySelector("body");
+var root = document.querySelector(":root");
+
+// Update year
+var years = ["1990", "1992", "1995", "1996", "1996", "1998", "2002", "2003", "2015", "2019"]
+var yearIndex = 0;
+
+// Buttons (eieren)
+var SMWbutton = document.querySelector("ol li:first-of-type button");
+var SMKbutton = document.querySelector("ol li:nth-of-type(2) button");
+var SMW2button = document.querySelector("ol li:nth-of-type(3) button");
+var SMRPGbutton = document.querySelector("ol li:nth-of-type(4) button");
+var SM64button = document.querySelector("ol li:nth-of-type(5) button");
+var YSbutton = document.querySelector("ol li:nth-of-type(6) button");
+var SMSbutton = document.querySelector("ol li:nth-of-type(7) button");
+var SM64DSbutton = document.querySelector("ol li:nth-of-type(8) button");
+var YWWbutton = document.querySelector("ol li:nth-of-type(9) button");
+var YCWbutton = document.querySelector("ol li:nth-of-type(10) button");
+
+var position = 0;
+
+// Rotate carousel
+leftButton.addEventListener("click", rotateLeft);
+rightButton.addEventListener("click", rotateRight);
+
+function rotateLeft() {
+    // Increment position before setting property, because value is otherwise updated aftwerwards
+    position++
+    root.style.setProperty('--position', position);
+    
+    previousYear()
+}
+
+function rotateRight() {
+    position--
+    root.style.setProperty('--position', position);
+    // setCurrentYear();
+    console.log(position);
+
+    nextYear()
+
+}
 
 
+// function setCurrentYear() {
+//     if (position < 0) {
+//         position = years.length - 1;
+//     } 
+//     if (position > years.length - 1) {
+//         position = 0;
+//     }
+//     const year = years[years.length - 1 - position];
+//     yearText.innerText = year;
+// }
+
+
+function previousYear(){
+    yearIndex--;
+    // Wrap the position to the end if it goes out of bounds
+    if (yearIndex < 0) {
+        yearIndex = years.length - 1;
+    }
+    updateYear();
+}
+
+function nextYear(){
+    yearIndex++;
+    // Wrap the position to the beginning if it goes out of bounds
+    if (yearIndex > years.length - 1) {
+        yearIndex = 0;
+    }
+    updateYear();
+}
+
+function updateYear(){
+    // Update the year to the correct year according to the yearindex
+    yearText.innerText = years[yearIndex];
+}
+
+// Code inspired from https://www.tutorialspoint.com/detecting-arrow-key-presses-in-javascript
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            console.log("Left key is pressed.");
+            rotateLeft()
+            break;
+        case 39:
+            console.log("Right key is pressed.");
+            rotateRight()
+            break;
+    }
+ };
